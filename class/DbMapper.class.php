136330120316ee76b17184dbeb196ae7839d1476
Dmitry Cheva <dmitry.cheva@gmail.com>
2012-06-06 22:56:14 +0300

    * @cheva этопиздец. такого линейного говнокода * я не видел со времён бейсика на спектрумах * @refactor блеать!!!

--- class/DbMapper.class.php
+++ class/DbMapper.class.php
@@ -15,6 +15,10 @@
  * You should have received a copy of the GNU General Public License along with 
  * this program; if not, write to the Free Software Foundation, Inc., 
  * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ * 
+ * @cheva этопиздец. такого линейного говнокода 
+ * я не видел со времён бейсика на спектрумах
+ * @refactor блеать!!!
  */
 
 class DbMapper {
